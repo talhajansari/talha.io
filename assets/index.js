9438ccd8d4046fc4062f0c5994f0e1813c7a00c5
Talha Ansari <talhajansari@Teejays-Air.fios-router.home>
2016-10-15 17:46:53 -0400

    Refactored JS code and added session checking for faster initiating of the page if the page has just been visited.

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-var headingText = 'Hello, ^1000 I am <span class="orange">Talha.</span>';
+var headingText = ['Hello, ^1000 I am <span class="orange">Talha.</span>'];
 var workText = ['I am a <span class="blue">coder,</span> ^1000at <a href="https://www.nextjump.com"><span class="orange">Next Jump</span></a>']
 var subworkText = ['I like <span class="blue">chicken tikka</span> and <span class="orange">nihari^1000.</span>']
 
@@ -8,40 +8,51 @@ $(document).ready(
   }
 );
 function initalize() {
-  $('#heading-typed').typed({
-    strings: [headingText],
-    typeSpeed: 0,
-    showCursor: true,
-    callback: function() {
-      $('.heading-container .typed-cursor').hide();
-    }
-  });
+  var makeInstant;
+  if (isSessionVisited()) { makeInstant=true; } else { makeInstant=false}
 
-  $('#work-typed').typed({
-    strings: workText,
-    startDelay: 3000,
-    typeSpeed: 0,
-    showCursor: true,
-    preStringTyped: function() {
-      $('.work-container .typed-cursor').show();
-    },
-    callback: function() {
-      $('.work-container .typed-cursor').hide();
-    }
-  });
-  $('#subwork-typed').typed({
-    strings: subworkText,
-    startDelay: 6000,
-    typeSpeed: 0,
-    showCursor: true,
-    preStringTyped: function() {
-      $('.subwork-container .typed-cursor').show();
-    },
-    callback: function() {
-      setTimeout( function() { $('.contact-bar').fadeIn(800); }, 2000)
-    }
-  });
-  $('.contact-bar').hide();
-  $('.work-container .typed-cursor').hide(); // This comes after the typed.js initialization above, but because of the delay mentioned in options above, it is processed before the typing.
-  $('.subwork-container .typed-cursor').hide(); // This comes after the typed.js initialization above, but because of the delay mentioned in options above, it is processed before the typing.
+  initiateTyped($('#heading-typed'), headingText, 0, makeInstant);
+  initiateTyped($('#work-typed'), workText, 3000, makeInstant);
+  initiateTyped($('#subwork-typed'), subworkText, 6000, makeInstant);
+
+  setTimeout( function() { $('.contact-img').fadeIn(800); }, 500)
+
 }
+
+var initiateTyped = function(element, strings, delay, instant) {
+  if (typeof delay === 'undefined') { delay = delay*0.1; }
+  if (typeof instant === 'undefined') { instant = true; }
+  if (instant) {
+    delay = 0;
+    typeSpeed=-1000000;
+    for (var i = 0; i < strings.length; i++) {
+      strings[i] = strings[i].replace(/\^\d+/g,'');
+      console.log(strings[i]);
+    }
+
+  } else {
+    typeSpeed=0;
+  }
+  element.typed({
+    strings: strings,
+    startDelay: delay,
+    typeSpeed: typeSpeed,
+    showCursor: true,
+    preStringTyped: function() {
+      element.next().show();
+    },
+    callback: function() {
+      element.next().hide();
+    }
+  });
+  element.next().hide();
+};
+
+var isSessionVisited = function() {
+    if (document.cookie.indexOf("visited") >= 0) {
+        return true;
+    } else {
+        document.cookie = "visited";
+        return false;
+    }
+};
