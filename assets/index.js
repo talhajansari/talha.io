facec0de42486fd7185c285a8d95ac6d612c75ec
Talha Ansari <talhajansari@hotmail.com>
2019-08-30 09:48:49 -0400

    V2 of talha.io with more convo

--- assets/index.js
+++ assets/index.js
@@ -1,41 +1,105 @@
-var headingText = ['Hello, ^1000 I am <span class="orange">Talha.</span>'];
-var workText = ['I am a <span class="blue">coder,</span> ^1000at <a href="https://www.nextjump.com"><span class="orange">Next Jump.</span></a>']
-var subworkText = ['I like <span class="blue">chicken tikka</span> and <span class="orange">nihari^1000.</span>']
+const newTyper = (duration, strs) => {
+  return {
+    Text: strs,
+    Duration: duration
+  };
+};
+const typedText = {
+  heading: newTyper(1000, [
+    'Hello, ^1000 I am <span class="orange">Talha.</span>'
+  ]),
+  work: newTyper(4000, [
+    'I am a <span class="blue">coder,</span> ^1000at <a href="https://www.glassdoor.com/Overview/Working-at-Devoted-Health-EI_IE2183017.11,25.htm" target="_blank"><span class="purple underlineOnHover">Devoted Health.</span></a>'
+  ]),
+  other: newTyper(7000, [
+    'I like <span class="blue">lamb chops</span> and <a href="https://en.wikipedia.org/wiki/Nihari" target="_blank"><span class="orange">nihari^1000.</span></a>^1000',
+    'I also like ^500tinkering with ^500<a href="https://golang.org/" target="_blank">Go.</a>^1000',
+    'You can check out^500 my Github at ^500<a href="https://github.com/teejays" target="_blank">github.com/teejays.</a>'
+  ]),
+  convo: newTyper(20000, [
+    "^10000",
+    "I am not going to be^500 typing forever,^1000 in case you are wondering...^5000",
+    "^...^500",
+    "I meant it.^1000.. I have things to do..^1000 Important things!",
+    "Like^400...^500playing Fifa...^500 :/ ^2000",
+    "Aghh... well,^1000 if you don't trust me,^500 you can check^500 the source code for this webpage.^2000",
+    'You can see in the <a href="assets/index.js" target="_blank">index.js</a> file that^500 this is literally the ^1000(second) last line I will type.^3000',
+    "This one being the last. Bubye now!^1000 :) ^2000",
+    "^18000",
+    "(the cursor will keep on flashing...^500 doesn't mean I am going to type)^1000",
+    "^8000",
+    "...fine, ^500 I lied about that being the last line.^1000",
+    "But hey! you never checked the source code...^1000 did you?^1000",
+    "I think^100But hey! you never check^100The cursor is going to keep on flashing... doesn't mean I am ^100But hey! you never checked the^100But hey! you never checked the^2000",
+    "S^500h^500i^500t^500t^500!^500!^1000 Seems like there is a bug in this code! :( ^2000 I should go fix it...^1000000",
+    "^1000 "
+  ])
+};
 
-$(document).ready(
-  function() {
-    $( "#bg-img" ).imagesLoaded( function() {
-      $('body').css('background-image', 'url('+'assets/bg_paper1.png'+')');
-      $('div.body_container').show();
-      initalize();
-    });
-  }
-);
+$(document).ready(function() {
+  $("#bg-img").imagesLoaded(function() {
+    $("body").css("background-image", "url(" + "assets/bg_paper1.png" + ")");
+    $("div.body_container").show();
+    initalize();
+  });
+});
+
 function initalize() {
-  var makeInstant;
-  if (isSessionVisited()) { makeInstant=true; } else { makeInstant=false}
+  // Fade In the Contact Bar
+  setTimeout(function() {
+    $(".contact-bar").fadeIn(800);
+  }, 100);
 
-  initiateTyped($('#heading-typed'), headingText, 0, makeInstant);
-  initiateTyped($('#work-typed'), workText, 3000, makeInstant);
-  initiateTyped($('#subwork-typed'), subworkText, 6000, makeInstant);
+  // Fade In the Profile Picture
+  setTimeout(function() {
+    $(".profile-img-container").fadeIn(800);
+  }, 100);
 
-  setTimeout( function() { $('.contact-img').fadeIn(800); }, 500)
-
+  // Initialize the typings
+  // If we have already visited the site from this device, let's not waste time printing things slowly.
+  initiateTyped(
+    $("#heading-typed"),
+    typedText.heading.Text,
+    typedText.heading.Duration,
+    isSessionVisited()
+  );
+  initiateTyped(
+    $("#work-typed"),
+    typedText.work.Text,
+    typedText.work.Duration,
+    isSessionVisited()
+  );
+  initiateTyped(
+    $("#subwork-typed"),
+    typedText.other.Text,
+    typedText.other.Duration,
+    isSessionVisited()
+  );
+  initiateTyped(
+    $("#convo-typed"),
+    typedText.convo.Text,
+    typedText.convo.Duration,
+    false
+  );
 }
 
 var initiateTyped = function(element, strings, delay, instant) {
-  if (typeof delay === 'undefined') { delay = delay*0.1; }
-  if (typeof instant === 'undefined') { instant = true; }
+  if (typeof delay === "undefined") {
+    delay = delay * 0.1;
+  }
+  if (typeof instant === "undefined") {
+    instant = true;
+  }
   if (instant) {
     delay = 0;
-    typeSpeed=-1000000;
+    typeSpeed = -1000000;
     for (var i = 0; i < strings.length; i++) {
-      strings[i] = strings[i].replace(/\^\d+/g,'');
+      strings[i] = strings[i].replace(/\^\d+/g, "");
     }
-
   } else {
-    typeSpeed=0;
+    typeSpeed = -10;
   }
+
   element.typed({
     strings: strings,
     startDelay: delay,
@@ -52,10 +116,10 @@ var initiateTyped = function(element, strings, delay, instant) {
 };
 
 var isSessionVisited = function() {
-    if (document.cookie.indexOf("visited") >= 0) {
-        return true;
-    } else {
-        document.cookie = "visited";
-        return false;
-    }
+  if (document.cookie.indexOf("visited") >= 0) {
+    return true;
+  } else {
+    document.cookie = "visited";
+    return false;
+  }
 };
